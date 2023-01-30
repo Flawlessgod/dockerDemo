5803f5074731bf34d76d64d82208b6c7a7343898
Christophe Gerard <chg502@usask.ca>
2023-01-30 17:00:10 -0600

    tutorial recommendations

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-'use strict';
+"use strict";
 
 // load package
-const express = require('express');
+const express = require("express");
 const bodyParser = require("body-parser");
 
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const PORT = 3000;
+const HOST = "0.0.0.0";
 const app = express();
- 
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/sayHello', (req,res) => {
-        var name = req.body.name;
-        var response = new Object();
-        response.answer = "hello " + name;         
-        res.send(JSON.stringify(response));
-   });
+app.post("/sayHello", (req, res) => {
+  var name = req.body.name;
+  var response = new Object();
+  response.answer = "hello " + name;
+  res.send(JSON.stringify(response));
+});
 
- app.use('/', express.static('pages'));
+app.use("/", express.static("pages"));
 
 app.listen(PORT, HOST);
 
-console.log('up and running');
+console.log(`Running on http://${HOST}:${PORT}`);
